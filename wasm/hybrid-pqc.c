57b69eba91adc6035a1229f177f5643121edc9cb
Blake Hartin <blakehartin@blakehartin>
2023-03-29 04:23:09 +0000

    Fix mapping to hybrid-pqc for wasm

--- wasm/hybrid-pqc.c
+++ wasm/hybrid-pqc.c
@@ -21,17 +21,22 @@ uint64_t* mem_alloc_long_long(size_t size) {
 EMSCRIPTEN_KEEPALIVE
 void mem_free(void* ptr, size_t size) {
 	memset(ptr, 0, size);
-	free(ptr);
+ 	free(ptr);
 }
 
 EMSCRIPTEN_KEEPALIVE
 int dp_sign_keypair(unsigned char* pk, unsigned char* sk) {
-
-	unsigned char *pkk = malloc((32 + 897) * sizeof(unsigned char));
-	unsigned char *skk = malloc((64 + 1281 + 897) * sizeof(unsigned char));
-
 	return crypto_sign_falcon_ed25519_keypair(pk, sk);
-	
+}
+
+EMSCRIPTEN_KEEPALIVE 
+int dp_sign(unsigned char* sm, size_t* smlen, const unsigned char* m, size_t mlen, const unsigned char* sk){
+	return crypto_sign_falcon_ed25519(sm, smlen, m, mlen, sk);
+}
+
+EMSCRIPTEN_KEEPALIVE 
+int dp_sign_verify(unsigned char* m, size_t mlen, const unsigned char *sm, size_t smlen, const unsigned char *pk){
+ 	return crypto_verify_falcon_ed25519(m, mlen, sm, smlen, pk);
 }
 
 #ifdef __cplusplus
