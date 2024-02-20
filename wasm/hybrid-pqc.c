e165b04d649f45c07c84dcaca37615da8f4fe370
Steve Harrington <steveharrington1206@gmail.com>
2024-02-20 00:20:29 -0800

    add web assembly workflow

--- wasm/hybrid-pqc.c
+++ wasm/hybrid-pqc.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../hybrid/hybrid.h"
+#include "../hybrid-dilithium-sphincs/hybrid.h"
 #include "../random/randombytes.h"
 
 #ifdef __cplusplus
@@ -27,22 +27,22 @@ void mem_free(void* ptr, size_t size) {
 
 EMSCRIPTEN_KEEPALIVE
 int dp_sign_keypair_seed(unsigned char* pk, unsigned char* sk, unsigned char* seed) {
-	return crypto_sign_falcon_ed25519_keypair_seed(pk, sk, seed);
+	return crypto_sign_dilithium_ed25519_sphincs_keypair_seed(pk, sk, seed);
 }
 
 EMSCRIPTEN_KEEPALIVE
 int dp_sign_keypair(unsigned char* pk, unsigned char* sk) {
-	return crypto_sign_falcon_ed25519_keypair(pk, sk);
+	return crypto_sign_dilithium_ed25519_sphincs_keypair(pk, sk);
 }
 
 EMSCRIPTEN_KEEPALIVE 
 int dp_sign(unsigned char* sm, size_t* smlen, const unsigned char* m, size_t mlen, const unsigned char* sk){
-	return crypto_sign_falcon_ed25519(sm, smlen, m, mlen, sk);
+	return crypto_sign_compact_dilithium_ed25519_sphincs(sm, smlen, m, mlen, sk);
 }
 
 EMSCRIPTEN_KEEPALIVE 
 int dp_sign_verify(unsigned char* m, size_t mlen, const unsigned char *sm, size_t smlen, const unsigned char *pk){
- 	return crypto_verify_falcon_ed25519(m, mlen, sm, smlen, pk);
+ 	return crypto_verify_compact_dilithium_ed25519_sphincs(m, mlen, sm, smlen, pk);
 }
 
 EMSCRIPTEN_KEEPALIVE
